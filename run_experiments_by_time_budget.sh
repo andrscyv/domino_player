674eeb9eafd55b9eedd4fbc0a6cf9e0d0402f94e
Andres Cruz <andrscyv@gmail.com>
2021-04-24 00:12:21 -0500

    add run experiments by time budget script

---
+++ run_experiments_by_time_budget.sh
@@ -0,0 +1,37 @@
+mkdir ./experiments
+num_games=$1
+
+#Prueba que mcts es correcto
+time=0.0001
+for time in 0.0001 0.0005 0.001 0.005 0.01 0.05 0.1
+do 
+    file=mcts${time}_greedy
+    echo $file
+    python3 simulation.py --time-budget $time --teams mcts greedy --file ./experiments/$file.pickle $num_games > ./experiments/$file.txt
+done
+
+
+# #Sensibilidad de mcts a num_simulations
+# file=mctsw_mctsss
+# echo $file
+# python3 simulation.py --simulations 5 --teams mcts_w mcts_ss --file ./experiments/$file.pickle $num_games > ./experiments/$file.txt
+# file=mctsm_mctsss
+# echo $file
+# python3 simulation.py --simulations 5 --teams mcts_m mcts_ss --file ./experiments/$file.pickle $num_games > ./experiments/$file.txt
+# file=mctss_mctsss
+# echo $file
+# python3 simulation.py --simulations 5 --teams mcts_s mcts_ss --file ./experiments/$file.pickle $num_games > ./experiments/$file.txt
+# file=mctsss_mctsss
+# echo $file
+# python3 simulation.py --simulations 5 --teams mcts_ss mcts_ss --file ./experiments/$file.pickle $num_games > ./experiments/$file.txt
+# file=mcts300_mctsss
+# echo $file
+# python3 simulation.py --simulations 300 --teams mcts mcts_ss --file ./experiments/$file.pickle $num_games > ./experiments/$file.txt
+
+# #Desempeño minimo
+# file=pimc20-100_greedy
+# echo $file
+# python3 simulation.py --simulations 20 --samples 100 --teams pimc greedy --file ./experiments/$file.pickle $num_games > ./experiments/$file.txt
+
+
+ 
