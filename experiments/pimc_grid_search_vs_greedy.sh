7dfea2624b0eb60e8b3dd1e1c4c8c93c822a087c
Andres Cruz <andrscyv@gmail.com>
2022-04-18 22:49:08 -0500

    feat: add grid search experiments

---
+++ experiments/pimc_grid_search_vs_greedy.sh
@@ -0,0 +1,15 @@
+num_games=$1
+timestamp=$(date +"%s")
+
+num_samples_list=(10000 2000 1000 200 100 20 10 2 1)
+time_budget_list=(0.0001 0.0005 0.001 0.005 0.01 0.05 0.1 0.5 1)
+for idx in {1..$#num_samples_list}
+do 
+    num_samples=${num_samples_list[$idx]}
+    time_budget=${time_budget_list[$idx]}
+    echo $num_samples and $time_budget
+    python3 ../simulation.py --teams pimc_${time_budget}_${num_samples} greedy $num_games 
+done
+
+
+ 
