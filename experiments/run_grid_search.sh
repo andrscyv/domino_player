f2ad706baf0eb2ef9c97e451a57ac5375187703e
Andres Cruz <andrscyv@gmail.com>
2022-07-07 14:05:38 -0500

    refactor: put budget lists in single script for grid search

--- experiments/run_grid_search.sh
+++ experiments/run_grid_search.sh
@@ -3,6 +3,8 @@
 # nohup ./run_experiments.sh $1 &
 # echo $! > pid.txt
 
-source run_experiment.sh pimc_grid_search_vs_greedy.sh $1 && \
-source run_experiment.sh pimc_grid_search_vs_mcts_1.sh $1
+num_samples_list=(500 1000 1500 2000 2500 3000 3500 4000 4500 5000 5500 6000 6500 7000 7500 8000 8500 9000 9500 10000)
+time_budget_list=(0.00200 0.00100 0.00067 0.00050 0.00040 0.00033 0.00029 0.00025 0.00022 0.00020 0.00018 0.00017 0.00015 0.00014 0.00013 0.00013 0.00012 0.00011 0.00011 0.00010)
+source pimc_grid_search_vs_greedy.sh $1 && \
+source pimc_grid_search_vs_mcts_1.sh $1
 
