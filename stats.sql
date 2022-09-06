ce4eff83ba1b43aabecad9d2fa6a5fc976b0a103
Andres Cruz <andrscyv@gmail.com>
2022-09-05 21:18:35 -0500

    feat: add sql queries for grid search graphs

--- stats.sql
+++ stats.sql
@@ -4,7 +4,7 @@ select
 	e.p1 as team_1,
 	e.p2 as team_2,
 	e.num_games,
-	count(distinct g.game_id)*1.0/e.num_games as percent_wins_team_1
+	(count(distinct g.game_id)*1.0/e.num_games)*100 as percent_wins_team_1
 from game g
 inner join experiment e on e.experiment_id = g.experiment_id
 where g.winner = 1
@@ -28,3 +28,45 @@ from play p
 inner join game g on g.game_id = p.game_id 
 inner join experiment e on e.experiment_id = g.experiment_id 
 group by e.experiment_id 
+
+
+-- Data graph grid search PIMC vs Greedy
+select 
+	e.p1 as algo,
+	(count(distinct g.game_id)*1.0/e.num_games)*100 as percent_wins_algo,
+	CAST(replace(e.p1, rtrim(e.p1, replace(e.p1, '_', '')), '') as INTEGER) as pimc_num_simulations
+from game g
+inner join experiment e on e.experiment_id = g.experiment_id
+where g.winner = 1 and g.experiment_id >= 66 and g.experiment_id <= 85
+group by g.experiment_id 
+UNION 
+select 
+	e.p1 as algo,
+	(count(distinct g.game_id)*1.0/e.num_games)*100 as percent_wins_algo,
+	CAST(replace(e.p1, rtrim(e.p1, replace(e.p1, '_', '')), '') as INTEGER) as pimc_num_simulations
+from game g
+inner join experiment e on e.experiment_id = g.experiment_id
+where g.winner = 1 and g.experiment_id >= 1 and g.experiment_id <= 20
+group by g.experiment_id 
+order by pimc_num_simulations
+
+-- Data graph grid search PIMC vs Strong player
+select 
+	e.p1 as algo,
+	(count(distinct g.game_id)*1.0/e.num_games)*100 as percent_wins_algo,
+	CAST(replace(e.p1, rtrim(e.p1, replace(e.p1, '_', '')), '') as INTEGER) as pimc_num_simulations
+from game g
+inner join experiment e on e.experiment_id = g.experiment_id
+where g.winner = 1 and g.experiment_id >= 87 and g.experiment_id <= 106
+group by g.experiment_id 
+UNION 
+select 
+	e.p1 as algo,
+	(count(distinct g.game_id)*1.0/e.num_games)*100 as percent_wins_algo,
+	CAST(replace(e.p1, rtrim(e.p1, replace(e.p1, '_', '')), '') as INTEGER) as pimc_num_simulations
+from game g
+inner join experiment e on e.experiment_id = g.experiment_id
+where g.winner = 1 and g.experiment_id >= 21 and g.experiment_id <= 40
+group by g.experiment_id 
+order by pimc_num_simulations
+
